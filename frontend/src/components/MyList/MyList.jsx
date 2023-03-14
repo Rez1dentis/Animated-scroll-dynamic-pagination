3ed78524f52a2c57169e60b5701492a7f9db824c
Ivan Kosenko <jean.kosenko@gmail.com>
2023-03-14 12:25:58 +0300

    refactor

--- frontend/src/components/MyList/MyList.jsx
+++ frontend/src/components/MyList/MyList.jsx
@@ -1,59 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Aos from 'aos';
-import style from './mylist.module.css';
 import 'aos/dist/aos.css';
+import styles from './mylist.module.css';
 
 export default function MyList() {
   const [users, setUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [fetching, setFetching] = useState(true);
-
-  const scrollHandler = (e) => {
-    if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 10) {
-      setFetching(true);
-    }
-  };
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
-    if (fetching) {
-      console.log(fetching);
-      axios.get(`https://6372695e025414c6370f15bd.mockapi.io/users?limit=20&page=${currentPage}`)
-        .then((res) => {
-          setUsers([...users, ...res.data]);
-          setCurrentPage((prev) => prev + 1);
-        })
-        .finally(() => setFetching(false));
-    }
-  }, [fetching]);
-
-  useEffect(() => {
-    document.addEventListener('scroll', scrollHandler);
-    return function () {
-      document.removeEventListener('scroll', scrollHandler);
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get(
+          `https://6372695e025414c6370f15bd.mockapi.io/users?limit=20&page=${currentPage}`,
+        );
+        setUsers((prevUsers) => [...prevUsers, ...res.data]);
+        setCurrentPage((prevPage) => prevPage + 1);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsFetching(false);
+      }
     };
-  });
+
+    if (isFetching) {
+      fetchUsers();
+    }
+  }, [currentPage, isFetching]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isAtBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight;
+      if (isAtBottom && !isFetching) {
+        setIsFetching(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isFetching]);
 
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, []);
 
   return (
-
-    <div className={style.listBlock}>
-      {users?.map((user) => (
-        <ul className={style.list} key={user.id}>
-          <li data-aos="fade-up">
-            {user.id}
-            .
-            {' '}
-            {user.name}
-            {' '}
-            {user.surname}
+    <div className={styles.listBlock}>
+      {users.map((user) => (
+        <ul className={styles.list} key={user.id} data-aos="fade-up">
+          <li>
+            {user.id}. {user.name} {user.surname}
           </li>
         </ul>
       ))}
     </div>
-
   );
 }
